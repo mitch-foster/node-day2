1f286b8bfb13842018429ef23456d89c1aa28223
Mitchel Foster <mitchfoster3@gmail.com>
2017-06-28 13:55:31 -0600

    added gitignore

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const config = require('./config');
 const app = express();
-const port = 3001;
+const port = 3000;
 
 
 app.use(bodyParser.json());
